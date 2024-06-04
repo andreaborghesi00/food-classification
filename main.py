c070d7dabb8393768ff513509ec02836df94ac92
mirkomorello <mirkomorello97@gmail.com>
2024-06-04 14:21:36 +0200

    removed notebook and added py

---
+++ main.py
@@ -0,0 +1,54 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ----
+# # <center>Dataset Preprocessing
+#
+
+# %%
+
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if not os.path.exists('dataset'):
+    os.makedirs('dataset')
+
+# download the dataset if the file does not exist
+
+if not os.path.exists('dataset/train_info.csv'):
+    os.system('wget https://food-x.s3.amazonaws.com/annot.tar -O dataset/annot.tar')
+    os.system('tar -xvf dataset/annot.tar -C dataset')
+    os.system('rm dataset/annot.tar')
+
+if not os.path.exists('dataset/train'):
+    os.system('wget https://food-x.s3.amazonaws.com/train.tar -O dataset/train.tar')
+    os.system('tar -xvf dataset/train.tar -C dataset')
+    os.system('rm dataset/train.tar')
+
+if not os.path.exists('dataset/test'):
+    os.system('wget https://food-x.s3.amazonaws.com/test.tar -O dataset/test.tar')
+    os.system('tar -xvf dataset/test.tar -C dataset')
+    os.system('rm dataset/test.tar')
+    
+if not os.path.exists('dataset/val'):
+    os.system('wget https://food-x.s3.amazonaws.com/val.tar -O dataset/val.tar')
+    os.system('tar -xvf dataset/val.tar -C dataset')
+    os.system('rm dataset/val.tar')
+
+# %%
