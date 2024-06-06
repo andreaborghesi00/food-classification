966979c0acec0905930a04e90237b7fe80445656
Coso <00borghesiandrea@gmail.com>
2024-06-06 12:48:01 +0200

    fixed simple coso cnn, ready for training

--- main.py
+++ main.py
@@ -255,18 +255,21 @@ class simple_coso_CNN(Module):
         super(simple_coso_CNN, self).__init__()
         self.conv1 = Sequential(
             Conv2d(3, 8, kernel_size=3, stride=1, padding='same'),
+            GELU(),
             Conv2d(8, 32, kernel_size=3, stride=1, padding=1),
             GELU(),
             MaxPool2d(kernel_size=2, stride=2, padding=0)
         )
         self.conv2 = Sequential(
             Conv2d(32, 32, kernel_size=3, stride=1, padding='same'),
+            GELU(),
             Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             GELU(),
             MaxPool2d(kernel_size=2, stride=2, padding=0)
         )
         self.conv3 = Sequential(
             Conv2d(64, 64, kernel_size=3, stride=1, padding='same'),
+            GELU(),
             Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             GELU(),
             MaxPool2d(kernel_size=2, stride=2, padding=0)
@@ -274,6 +277,7 @@ class simple_coso_CNN(Module):
 
         self.conv4 = Sequential(
             Conv2d(128, 128, kernel_size=3, stride=1, padding='same'),
+            GELU(),
             Conv2d(128, 32, kernel_size=3, stride=1, padding=1),
             GELU(),
             MaxPool2d(kernel_size=2, stride=2, padding=0)
@@ -293,6 +297,7 @@ class simple_coso_CNN(Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
+        x = self.conv4(x)
         x = x.view(-1, 32*8*8)
         x = self.fc1(x)
         x = self.fc2(x)
@@ -311,10 +316,10 @@ def train(model, train_dl, val_dl, optimizer, criterion, epochs):
         running_loss = 0.0
         correct = 0
         total = 0
-        for i, data in enumerate(train_dl):
+        for i, data in tqdm(enumerate(train_dl)):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
-            
+
             optimizer.zero_grad()
             
             outputs = model(inputs)
